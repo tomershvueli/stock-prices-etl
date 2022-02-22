8e2d68de777f00109f8b321917ea5ecd6c644ccb
Tomer <tomer@shvueli.com>
2022-02-22 18:30:51 -0500

    fix: Leverage AlphaVantage's daily API instead of by the minute

--- src/services/alphaVantage.js
+++ src/services/alphaVantage.js
@@ -8,17 +8,17 @@ const { BASE_URL } = process.env;
 exports.dailyInfo = async (symbol, timestamp) => {
   const { data: apiResponseData } = await axios.get(
     BASE_URL
-    + '?function=TIME_SERIES_INTRADAY&outputsize=full'
-    + `&symbol=${symbol}&interval=1min&apikey=${API_KEY}`
+    + '?function=TIME_SERIES_DAILY&outputsize=full'
+    + `&symbol=${symbol}&apikey=${API_KEY}`
   );
 
   return {
-    data: apiResponseData['Time Series (1min)'],
+    data: apiResponseData['Time Series (Daily)'],
     meta: {
       ...apiResponseData['Meta Data'],
       timestamp: timestamp,
       date: timestamp.format('YYYY-MM-DD'),
-      dataLenght: Object.keys(apiResponseData['Time Series (1min)']).length
+      dataLenght: Object.keys(apiResponseData['Time Series (Daily)']).length
     }
   };
 };
